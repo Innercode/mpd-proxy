babc36a047865b572db204cfa9225a660d6cd4e9
Rienus Schot <rienus@innercode.nl>
2015-12-07 21:31:30 +0100

    Adding dotenv to the app for config options

--- app.php
+++ app.php
@@ -3,6 +3,11 @@ if (!$loader = include __DIR__.'/vendor/autoload.php') {
     die('You must set up the project dependencies.');
 }
 
+//Init dotenv
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+
+//Init console
 $app = new \Symfony\Component\Console\Application();
 $app->add(new App\Commands\MpdProxyCommand());
 $app->run();
